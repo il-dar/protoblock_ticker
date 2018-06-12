e9f074fa6cf557383c8946989cde8ebc2d632082
il-dar <ildarildar@protonmail.com>
2018-06-11 22:59:39 -0400

    React added. Need to solve COR api issues

--- app/javascript/packs/tickerApp/components/landingPage.jsx
+++ app/javascript/packs/tickerApp/components/landingPage.jsx
@@ -1,11 +1,34 @@
 import React from 'react';
 
 class LandingPage extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      week: "n/a",
+    };
+  }
+
+  componentDidMount(){
+    console.log("request being made to API")
+    var requestWeek = new Request('https://158.222.102.83:4545/week',{
+      method: 'GET',
+      headers: new Headers({
+        'Content-Type': 'application/plain'
+      })
+    })
+    fetch(requestWeek)
+      .then(results =>{
+        return results.json();
+      }).then(weekFetch =>{
+        console.log("weekFetch")
+      })
+  }
   render() {
     return(
       <div>
-        <h1>Hello World</h1>
+        <h1>Welcome to Protoblock</h1>
       </div>
+
     )
   }
 }
---
+++ app/javascript/packs/tickerApp/components/schedule.jsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+class Schedule extends React.Component {
+  constructor () {
+    super();
+
+  }
+    
+}
